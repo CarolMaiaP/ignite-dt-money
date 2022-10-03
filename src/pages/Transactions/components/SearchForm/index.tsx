758593165573610ab154358cc10d5db165f2fde0
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-03 18:24:04 -0300

    styles: search form styles added

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,14 +1,15 @@
 import {MagnifyingGlass} from 'phosphor-react'
+import { SearchFormContainer } from './styles'
 
 export function SearchForm(){
   return(
-    <form>
+    <SearchFormContainer>
       <input type="text" placeholder="Busque por transações" />
 
       <button type="submit">
         <MagnifyingGlass size={20}/>
         Buscar
       </button>
-    </form>
+    </SearchFormContainer>
   )
 }
