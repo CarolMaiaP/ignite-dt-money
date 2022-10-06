04fb0adfd95eb666e79ea207a179802920a06225
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-10-06 15:18:16 -0300

    feat: disable search form button when submitting data

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -11,11 +11,19 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm(){
-  const { register, handleSubmit } = useForm<SearchFormInputs>({
+  const { 
+    register,
+    handleSubmit,
+    formState: {
+      isSubmitting,
+    } 
+  } = useForm<SearchFormInputs>({
     resolver: zodResolver(searchFormSchema)
   });
 
-  function handleSearchTransactions(data: SearchFormInputs){
+  async function handleSearchTransactions(data: SearchFormInputs){
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
     console.log(data);
   }
 
@@ -27,7 +35,7 @@ export function SearchForm(){
         {...register('query')}
       />
 
-      <button type="submit">
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20}/>
         Buscar
       </button>
